7fb7adb70201d6940a01d1d04fd4b9beaeb3d7b8
wzy0766 <wzy0766@gmail.com>
2022-08-30 19:12:28 -0700

    Updated Homeworks

---
+++ Homework1/staticTopics.java
@@ -0,0 +1,14 @@
+package Homework1;
+
+/**
+ * 1)	Given a database table “Book” with columns (id, isbn, name, author, publish date), 
+ * define a java class that matches this table and then use a static block to initialize this table with some records.
+ * 
+ * 2)	Define a Java class “BookSeller” and then define a static inner class “Book”, and use a static method “sellBooks” 
+ * to initialize several books, and in the main method display all the books by calling the “sellBooks” method.
+ */
+public class staticTopics {
+    
+    
+
+}
